b1f70919fe68070bf06905c01673d5139cc031c4
Wilbert Liu <wilbertliu@gmail.com>
2018-02-17 22:35:58 +0700

    Add language declaration

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="en-US">
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {this.props.styleTags}
